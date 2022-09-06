af6a1d62b5a3c4cef1bb1adff692e108fe776bb4
Nalin Dwivedi <dwivedi.nalin2@gmail.com>
2022-09-06 07:56:15 +0530

    changes ci

--- cypress/pageobjects/pages/HomePage.js
+++ cypress/pageobjects/pages/HomePage.js
@@ -9,12 +9,12 @@ export default class HomePage extends BasePage{
     static VerifyHeader(){
         cy.get('*[class^="relative cursor-pointer w-[100px] h-[52px] sm:w-[150px] sm:h-[78px]"]').should("have.text",'')
     }
-    
+
 
     static VerifyFooter(){
         cy.get('*[class^="text-dark-200 w-[64%] font-medium sm:text-2xl sm:w-6/12 lg:w-5/12 2xl:w-4/12"]').should("have.text","We strive to develop best-in-class blockchain innovations")
     }
-
+    
     static VerifyCTA(){
 
     }
